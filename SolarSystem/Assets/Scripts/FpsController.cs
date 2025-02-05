558b6f31a3037e3a1caa09ff9d2d68695888927c
Harsh Sisale <harshvardhansisale.work@gmail.com>
2025-02-05 00:28:44 -0500

    final build

--- SolarSystem/Assets/Scripts/FpsController.cs
+++ SolarSystem/Assets/Scripts/FpsController.cs
@@ -4,44 +4,65 @@ using System.Numerics;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 using Quaternion = UnityEngine.Quaternion;
+
 public class FpsController : MonoBehaviour
 {
     public float sensitivity;
     public float slowspeed, normalSpeed, fastSpeed;
     float currentSpeed;
 
-    void Update(){
-
-        if(Input.GetMouseButton(1))
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
         {
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
             Movement();
             Rotation();
         }
-        else{
+        else
+        {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
         }
     }
-    
-    void Rotation(){
-        Vector3 mouseInput = new Vector3(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
-        transform.Rotate(-mouseInput* sensitivity * Time.deltaTime * 50);
+
+    void Rotation()
+    {
+        Vector3 mouseInput = new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0);
+        transform.Rotate(-mouseInput * sensitivity * Time.deltaTime * 50);
         Vector3 eulerRotation = transform.eulerAngles;
         transform.rotation = Quaternion.Euler(eulerRotation.x, eulerRotation.y, 0);
     }
-    void Movement(){
+
+    void Movement()
+    {
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        if(Input.GetKey(KeyCode.LeftShift)){
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
             currentSpeed = fastSpeed;
         }
-        else if(Input.GetKey(KeyCode.LeftControl)){
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
             currentSpeed = slowspeed;
         }
-        else{
+        else
+        {
             currentSpeed = normalSpeed;
         }
+
+        // Move normally
         transform.Translate(input * currentSpeed * Time.deltaTime);
+
+        // Move up when spacebar is pressed
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * slowspeed * Time.deltaTime);
+        }
+          if (Input.GetKey(KeyCode.LeftControl))
+        {
+            transform.Translate(Vector3.down * slowspeed * Time.deltaTime);
+        }
     }
 }
